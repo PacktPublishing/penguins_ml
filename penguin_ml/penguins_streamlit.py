e3ffc76f28691d2b2b447071fe6660e984e1ad0d
utkarsha-Packt <utkarshak@packt.com>
2021-07-10 00:30:54 +0530

    Update penguins_streamlit.py

--- penguin_ml/penguins_streamlit.py
+++ penguin_ml/penguins_streamlit.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import seaborn as sns 
-import matplotlib.pyplot as plt 
-import pandas as pd 
-import pickle 
-from sklearn.metrics import accuracy_score 
-from sklearn.ensemble import RandomForestClassifier 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
  
 st.title('Penguin Classifier')
@@ -13,7 +13,6 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
          " to get started!")
  
 password_guess = st.text_input('What is the Password?')
-if password_guess != st.secrets["password"]:
+if password_guess != 'streamlit_password':
   st.stop()
- 
 penguin_file = st.file_uploader('Upload your own penguin data')
