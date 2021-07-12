a917703f75b9f31e1bcfec6b163c9ab93fa348ab
utkarsha-Packt <utkarshak@packt.com>
2021-07-12 14:21:56 +0530

    Update penguins_streamlit.py

--- penguin_ml/penguins_streamlit.py
+++ penguin_ml/penguins_streamlit.py
@@ -1,21 +1,14 @@
 import streamlit as st
-import seaborn as sns 
-import matplotlib.pyplot as plt 
-import pandas as pd 
-import pickle 
-from sklearn.metrics import accuracy_score 
-from sklearn.ensemble import RandomForestClassifier 
-from sklearn.model_selection import train_test_split
- 
-st.title('Penguin Classifier') 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
 
-st.write("This app uses 6 inputs to predict the species of penguin using " 
+st.title('Penguin Classifier: A Machine Learning App')
+st.write("This app uses 6 inputs to predict the species of penguin using "
+         "a model built on the Palmer's Penguin's dataset. Use the form below"
+         " to get started!")
 
-         "a model built on the Palmer's Penguin's dataset. Use the form below" 
-
-         " to get started!") 
-
-  
 penguin_df = pd.read_csv('penguins.csv')
 rf_pickle = open('random_forest_penguin.pickle', 'rb')
 map_pickle = open('output_penguin.pickle', 'rb')
@@ -23,4 +16,3 @@ rfc = pickle.load(rf_pickle)
 unique_penguin_mapping = pickle.load(map_pickle)
 rf_pickle.close()
 map_pickle.close()
-
