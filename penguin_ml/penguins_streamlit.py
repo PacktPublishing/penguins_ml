f6416f08c6e1214ed83a0663da95e8a2151e701b
utkarsha-Packt <utkarshak@packt.com>
2021-07-12 14:16:15 +0530

    Update penguins_streamlit.py

--- penguin_ml/penguins_streamlit.py
+++ penguin_ml/penguins_streamlit.py
@@ -16,10 +16,11 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
          " to get started!") 
 
   
+penguin_df = pd.read_csv('penguins.csv')
+rf_pickle = open('random_forest_penguin.pickle', 'rb')
+map_pickle = open('output_penguin.pickle', 'rb')
+rfc = pickle.load(rf_pickle)
+unique_penguin_mapping = pickle.load(map_pickle)
+rf_pickle.close()
+map_pickle.close()
 
-password_guess = st.text_input('What is the Password?') 
-
-if password_guess != 'streamlit_is_great': 
-  st.stop() 
- 
-penguin_file = st.file_uploader('Upload your own penguin data')
